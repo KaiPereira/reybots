7a60f7ec5f00d7f7cf1b8f83696ee7f14be01fe9
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-10-27 07:04:16 -0700

    changed nav links to a's

--- components/Nav.js
+++ components/Nav.js
@@ -15,45 +15,45 @@ export default function Nav() {
     return (
         <>
             <nav>
-                <Link href="/reybots/#home">
+                <a href="/#home">
                     <div className="navGroup1">
                         <img src="/reybots/logo.svg" alt="reybots logo" />
                         <p>Reybots</p>
                     </div>
-                </Link>
+                </a>
                 <div className="navGroup2">
-                    <Link href="/reybots/#home">
+                    <a href="/#home">
                         <div className="navElementContainer">
                             <p>Home</p>
                         </div>
-                    </Link>
-                    <Link href="/reybots/#competitions">
+                    </a>
+                    <a href="/#competitions">
                         <div className="navElementContainer">
                             <p>Competitions</p>
                         </div>
-                    </Link>
-                    <Link href="/reybots/#sponsors">
+                    </a>
+                    <a href="/#sponsors">
                         <div className="navElementContainer">
                             <p>Sponsors</p>
                         </div>
-                    </Link>
-                    <Link href="/reybots/#about">
+                    </a>
+                    <a href="/#about">
                         <div className="navElementContainer">
                             <p>About</p>
                         </div>
-                    </Link>
-                    <Link href="/reybots/get-started">
+                    </a>
+                    <Link href="/get-started">
                         <div className="navElementContainer">
                             <p>Sponsor Us</p>
                         </div>
                     </Link>
                 </div>
                 <div className="navGroup2">
-                    <Link href="/reybots/#contact">
+                    <a href="/#contact">
                         <div className="navElementContainer">
                             <p>Contact</p>
                         </div>
-                    </Link>
+                    </a>
                 </div>
                 <svg width="27" height="22" viewBox="0 0 27 22" fill="none" xmlns="http://www.w3.org/2000/svg" class={dropdownState ? "navHamburger" : "navHamburger navHamburgerOpen"} onClick={() => changeDropdownStateFunction(!dropdownState)}>
                     <rect width="27" height="4" rx="2"></rect>
@@ -63,24 +63,24 @@ export default function Nav() {
             </nav>
             <div className={dropdownState ? "dropdown dropdownClosed" : "dropdown dropdownOpen"}>
                 <div className="dropdownElements">
-                    <Link href="/reybots/#home" onClick={() => changeDropdownStateFunction(!dropdownState)}>
+                    <a href="/#home" onClick={() => changeDropdownStateFunction(!dropdownState)}>
                         <p className="dropdownElement">Home</p>
-                    </Link>
-                    <Link href="/reybots/#competition" onClick={() => changeDropdownStateFunction(!dropdownState)}>
+                    </a>
+                    <a href="/#competition" onClick={() => changeDropdownStateFunction(!dropdownState)}>
                         <p className="dropdownElement">Competitions</p>
-                    </Link>
-                    <Link href="/reybots/#sponsors" onClick={() => changeDropdownStateFunction(!dropdownState)}>
+                    </a>
+                    <a href="/#sponsors" onClick={() => changeDropdownStateFunction(!dropdownState)}>
                         <p className="dropdownElement">Sponsors</p>
-                    </Link>
-                    <Link href="/reybots/#about" onClick={() => changeDropdownStateFunction(!dropdownState)}>
+                    </a>
+                    <a href="/#about" onClick={() => changeDropdownStateFunction(!dropdownState)}>
                         <p className="dropdownElement">About</p>
-                    </Link>
-                    <Link href="/reybots/get-started" onClick={() => changeDropdownStateFunction(!dropdownState)}>
+                    </a>
+                    <Link href="/get-started" onClick={() => changeDropdownStateFunction(!dropdownState)}>
                         <p className="dropdownElement">Sponsor Us</p>
                     </Link>
-                    <Link href="/reybots/#contact" onClick={() => changeDropdownStateFunction(!dropdownState)}>
+                    <a href="/#contact" onClick={() => changeDropdownStateFunction(!dropdownState)}>
                         <p className="dropdownElement">Contact</p>
-                    </Link>
+                    </a>
                     <div className="dropdownSocials">
                         <Link href="https://www.instagram.com/reynoldsreybots/" target="_blank" rel="noreferrer">
                             <button className="socialButton">
